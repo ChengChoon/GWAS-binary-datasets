709d3216bdc28a803a77ca848bf01cf5f0202eae
ChengChoon <cleanspring86@gmail.com>
2020-11-05 18:06:49 +0100

    edit

--- admixture_script.sh
+++ admixture_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-while (( run < 5 )); do  ### 5 is the default runs. To increase the accuracyof final result, we can run up to 100 runs for each K
+while (( run < 5 )); do # 5 is the default runs. To increase the accuracyof final result, we can run up to 100 runs for each K
 run=$(( run + 1 ));
-for K in 2 3 4 5  ; do  ### select a meaningful series of K. The more Ks, the longer the run it takes
+for K in 2 3 4 5  ; do # select a meaningful series of K. The more Ks, the longer the run it takes. #-jX:computation on X threads. #-s time:random seed to be generated from the current time
 ./admixture -s time --cv input.bed $K -j12 | tee log.K${K}.RUN$run.out;
 mv input.$K.P K$K.Run$run.P;
 mv input.$K.Q K$K.Run$run.Q;
