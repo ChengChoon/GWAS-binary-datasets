739f20f265a78901874e93d0b03456cad0e84f7d
ChengChoon <cleanspring86@gmail.com>
2020-11-05 18:01:51 +0100

    edit

--- admixture_script.sh
+++ admixture_script.sh
@@ -1,14 +1,10 @@
-
 #!/bin/bash
 
-while (( run < 5 )); do  ## we usually run 100 runs for each K for accuracy of the final results
+while (( run < 5 )); do  ### 5 is the default runs. To increase the accuracyof final result, we can run up to 100 runs for each K
 run=$(( run + 1 ));
-for K in 2 3 4 5  ; do  # select a meaningful series of K - the more Ks, the longer the run obviously
-./admixture -s time --cv ../73C_BISNP_filter_exclude_filtered_beagle_name_plink.bed $K -j12 | tee log.K${K}.RUN$run.out;
-mv 73C_BISNP_filter_exclude_filtered_beagle_name_plink.$K.P K$K.Run$run.P;
-mv 73C_BISNP_filter_exclude_filtered_beagle_name_plink.$K.Q K$K.Run$run.Q;
+for K in 2 3 4 5  ; do  ### select a meaningful series of K. The more Ks, the longer the run it takes
+./admixture -s time --cv input.bed $K -j12 | tee log.K${K}.RUN$run.out;
+mv input.$K.P K$K.Run$run.P;
+mv input.$K.Q K$K.Run$run.Q;
 done;
 done
-
-
-
